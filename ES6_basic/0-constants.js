1f6f28a1a725a9773bd4432bef5c7ca0f351ba94
Jose <josemayo3@gmail.com>
2024-07-17 13:03:42 -0400

    push 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,9 +1,7 @@
 function taskFirst() {
     const task = 'I prefer const when I can.';
-    console.log({task})
 }
 
 function taskNext() {
     let combination = 'But sometimes let';
-    console.log({combination});
 }
