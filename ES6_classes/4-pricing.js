db9bd6b7fcb49a2a065c2c5fd8e91ab24e404b57
Jose Mayo <mayitopr@hotmail.com>
2024-07-19 22:03:27 -0400

    task 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,7 +1,7 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount = '', currency) {
+  constructor(currency, amount = '') {
     this.amount = amount;
     this.currency = currency;
   }
