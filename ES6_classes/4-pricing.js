fea89999e90eab7aa67391807bf3cb38d6a3775e
Jose Mayo <josemayo3@gmail.com>
2024-07-19 22:39:53 -0400

    push 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,50 +1,39 @@
 import Currency from './3-currency';
 
+// Pricing class definition
 export default class Pricing {
-  constructor(amount = '', currency) {
-    this.amount = amount;
-    this.currency = currency;
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
   }
 
-  displayFullPrice() {
-    const code = (this.currency._code);
-    const name = (this.currency._name);
-    const money = `${this.amount} ${name} (${code})`;
-
-    return money;
-  }
-
-  static convertPrice(amount = 0, conversionRate = 0) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a string');
-    }
-
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('conversionRate must be a string');
-    }
-
-    return (amount * conversionRate);
-  }
-
+  // Getter for amount
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    this._amount = value;
+  // Setter for amount
+  set amount(newAmount) {
+    this._amount = newAmount;
   }
 
+  // Getter for currency
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
-    }
-    this._currency = value;
+  // Setter for currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price based on conversion rate
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }
