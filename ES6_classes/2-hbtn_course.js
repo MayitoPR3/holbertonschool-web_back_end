d036cb5a5bd250ee8eaff73e89a9ce9067b300b9
Jose Mayo <mayitopr@hotmail.com>
2024-07-19 21:44:00 -0400

    task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,38 +1,52 @@
-
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = '';
-        this._length = 0;
-        this._students = [];
-
-        this.name = name; // Calls the setter for validation
-        this.length = length; // Calls the setter for validation
-        this.students = students; // Calls the setter for validation
+  constructor(name, length, students) {
+    // Verify the types of attributes during object creation
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
 
-    get name() {
-        return this._name;
-    }
+    // Store attributes in underscore version
+    this._name = name;
+    this._length = length;
+    this._students = students.slice(); // Create a copy of students array
+  }
 
-    set name(newName) {
-        if (typeof newName !== 'string')
-        this._name = newName;
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = newName;
+  }
 
-    get length() {
-        return this._length;
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = newLength;
+  }
 
-    set length(newLength) {
-        this._length = newLength;
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every(s => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
-
-    get students() {
-        return this._students;
-    }
-
-    set students(newStudents) {
-        this._students = newStudents;
+    this._students = newStudents.slice(); // Update students with a copy of the new array
+  }
 }
-
-export default HolbertonCourse;
