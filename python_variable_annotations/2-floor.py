451947bfd27e8f756a44d35b1c8f10cd926494f6
Jose <josemayo3@gmail.com>
2024-08-15 12:27:44 -0400

    push 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import math
 
+"""Takes a float n and returns the floor of the float"""
+
 
 def floor(n: float) -> int:
     """
     Return the floor of the floar.
 
     Args:
-        n (float): The floating-point number to round.
+    n (float): The floating-point number to round.
 
     Returns:
-        int: The largest integer less than
+    int: The largest integer less than
     or equal to the given float.
     """
     return math.floor(n)
