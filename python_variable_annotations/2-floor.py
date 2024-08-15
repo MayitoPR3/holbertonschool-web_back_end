885237753af55052b0023ce106633ba4e900025a
Jose <josemayo3@gmail.com>
2024-08-15 12:05:04 -0400

    push 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def floor(n: float) -> float:
+    """
+    Return the floor of the floar.
+
+    Args:
+    n (float): The floating-point number to round.
+
+    Returns:
+    float: The rounded integer.
+    """
+    return float(n)
