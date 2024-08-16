44cf8b09311ca46a80cb1d7397d58f7ce7542d6f
Jose <josemayo3@gmail.com>
2024-08-15 23:13:08 -0400

    push 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,8 +4,8 @@
 from typing import Callable
 
 """
-Module "make_multiplier" to define a function
-that creates a multiplier function.
+function make_multiplier that takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier.
 """
 
 
