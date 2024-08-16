11e7bf2884792ffda43a10a4b0ab167888279152
Jose <josemayo3@gmail.com>
2024-08-15 22:49:11 -0400

    push 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" function sum_list takes a list of floats
+    and returns their sum"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Calculate the sum of a list of floating-point numbers.
+
+    Args:
+    input_list (list[float]): The list of floating-point numbers."""
+    return sum(input_list)
