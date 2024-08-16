84d1e2ae6b9641ade610508d744cad928482df5a
Jose <josemayo3@gmail.com>
2024-08-15 22:55:35 -0400

    push 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -10,7 +10,8 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     Calculate the sum of a list containing integers and floating-point numbers.
 
     Args:
-    mxd_lst (List[Union[int, float]]): The list containing integers and floating-point numbers.
+    mxd_lst (List[Union[int, float]]): The list containing 
+    integers and floating-point numbers.
 
     Returns:
     float: The sum of the integers and floating-point numbers in the list.
