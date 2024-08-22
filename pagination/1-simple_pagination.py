0937d4dd7ba3b2dc9d1260570f2af9f66977df0e
Jose <josemayo3@gmail.com>
2024-08-22 19:38:17 -0400

    PUSH TASKS

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-
-""" 1. Simple pagination """
+""" Simple pagination """
 
 import csv
+import math
 from typing import List
 
 index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """
-    Server class to paginate a database of
-    popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -19,24 +17,32 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
-            with open(self.DATA_FILE, newline='') as f:
+            with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                self.__dataset = [row for row in reader][1:]
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Return the appropriate page of the dataset.
+            - Use assert to verify that both arguments
+            are integers greater than 0.
+
+            - Use index_range to find the correct indexes to paginate
+            the dataset correctly and return the appropriate page
+            of the dataset (i.e. the correct list of rows).
         """
-        if not isinstance(page, int) or not isinstance(page_size, int):
-            raise TypeError("Both page and page_size must be integers.")
-        if page <= 0 or page_size <= 0:
-            raise ValueError("Both page and page_size must be greater than 0.")
+        assert(type(page) == int) and page > 0
+        assert(type(page_size) == int) and page_size > 0
 
         start, end = index_range(page, page_size)
-        data = self.dataset()
+        result = []
+        if start >= len(self.dataset()):
+            return result
 
-        return data[start:end]
+        result = self.dataset()
+        return result[start:end]
