903823a6a66af93f2a2e32b539ad75b50432d5f0
Jose <josemayo3@gmail.com>
2024-08-22 20:25:51 -0400

    PUSH TASKS

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 9. Insert a document in Python """
+"""9. Insert a document in Python"""
 
 def insert_school(mongo_collection, **kwargs):
     """
@@ -7,8 +7,6 @@ def insert_school(mongo_collection, **kwargs):
     collection based on kwargs
     """
 
-    # Insert the document and get the result
     result = mongo_collection.insert_one(kwargs)
 
-    # Return the _id of the new document
     return str(result.inserted_id)
