828e6c0c3433d67a43e1628d83383bb23e5fdce2
Jose <josemayo3@gmail.com>
2024-08-22 20:24:35 -0400

    PUSH TASKS

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,7 +6,6 @@ def list_all(mongo_collection):
     function that lists all documents in a collection
     """
 
-    documents = mongo_collection.find()
-    if documents.count() > 0:
-        return documents
-    return []
+    documents = list(mongo_collection.find())
+
+    return documents
