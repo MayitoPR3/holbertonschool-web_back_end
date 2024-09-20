a9e3456c25877ecf8368f6545b99fd38f07d3f38
Jose Mayo <josemayo3@gmail.com>
2024-09-19 21:57:21 -0400

    push tasks

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+
+const http = require('http');
+
+const port = 1245;
+
+// create a server object
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!'); // write a response to the client
+  res.end(); // end the response
+});
+app.listen(port);
+module.exports = app;
